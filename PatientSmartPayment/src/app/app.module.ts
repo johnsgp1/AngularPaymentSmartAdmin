7129f3f971b0df3ca036892ecc1318886e4b35b1
Johns George <johnsgp1@gmail.com>
2019-02-28 16:21:24 +0530

    Third

--- PatientSmartPayment/src/app/app.module.ts
+++ PatientSmartPayment/src/app/app.module.ts
@@ -8,14 +8,18 @@ import {appRoute} from './routes';
 import {FormsModule} from '@angular/forms';
 import {LoginService} from './shared/loginService/login.service';
 import {HttpClientModule} from '@angular/common/http';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { PaymentConfirmationComponent } from './payment-confirmation/payment-confirmation.component';
+import { PaymentResponseComponent } from './payment-response/payment-response.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    PaymentConfirmationComponent,
+    PaymentResponseComponent
   ],
   imports: [
     BrowserModule,
